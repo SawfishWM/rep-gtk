4499f014d8573eb8a12ab4e8a859d3626b237640
john <john>
1999-03-23 14:39:52 +0000

    cleaned up

--- examples/clist.jl
+++ examples/clist.jl
@@ -4,38 +4,45 @@ exec rep --batch "$0" "$@"
 
 (require 'gtk)
 
-(defvar titles ["name" "uid" "gid" "passwd" "gecos" "home" "shell"])
+(let*
+    ((titles ["name" "uid" "gid" "passwd" "gecos" "home" "shell"])
+     (window (gtk-window-new 'toplevel))
+     (scrolled-window (gtk-scrolled-window-new))
+     (clist (gtk-clist-new-with-titles titles)))
 
-(defvar window (gtk-window-new 'toplevel))
-(defvar scrolled-window (gtk-scrolled-window-new))
-(defvar clist (gtk-clist-new-with-titles titles))
+  (gtk-container-add window scrolled-window)
+  (gtk-container-add scrolled-window clist)
+  (gtk-scrolled-window-set-policy scrolled-window 'automatic 'automatic)
+  (gtk-widget-set-usize window 400 200)
+  (gtk-signal-connect
+   window "delete_event"
+   #'(lambda (w) (if (gtk-standalone-p)
+		     (gtk-exit)
+		   (gtk-widget-destroy w))))
 
-(gtk-container-add window scrolled-window)
-(gtk-container-add scrolled-window clist)
+  (let
+      ((i 0))
+    (while (< i 10)
+      (gtk-clist-append clist
+			(vector (format nil "record-%d" i)
+				(format nil "field1-%d" i)
+				(format nil "field2-%d" i)
+				(format nil "field3-%d" i)
+				(format nil "field4-%d" i)
+				(format nil "field5-%d" i)
+				(format nil "field6-%d" i)))
+      (setq i (1+ i))))
 
-(gtk-scrolled-window-set-policy scrolled-window 'automatic 'automatic)
+  (let
+      ((i 0))
+    (while (< i (length titles))
+      (gtk-clist-set-column-auto-resize clist i t)
+      (setq i (1+ i))))
 
-(gtk-widget-set-usize window 400 200)
-(gtk-signal-connect window "delete_event" #'(lambda () (gtk-exit)))
+  (gtk-widget-show-all window)
+  (when (gtk-standalone-p)
+    (gtk-main)))
 
-(let
-    ((i 0))
-  (while (< i 10)
-    (gtk-clist-append clist
-		      (vector (format nil "record-%d" i)
-			      (format nil "field1-%d" i)
-			      (format nil "field2-%d" i)
-			      (format nil "field3-%d" i)
-			      (format nil "field4-%d" i)
-			      (format nil "field5-%d" i)
-			      (format nil "field6-%d" i)))
-    (setq i (1+ i))))
-
-(let
-    ((i 0))
-  (while (< i (length titles))
-    (gtk-clist-set-column-auto-resize clist i t)
-    (setq i (1+ i))))
-
-(gtk-widget-show-all window)
-(gtk-main)
+;; Local variables:
+;; major-mode: lisp-mode
+;; End:
--- examples/hello.jl
+++ examples/hello.jl
@@ -6,17 +6,26 @@ exec rep --batch "$0" "$@"
 
 (require 'gtk)
 
-(setq window (gtk-window-new 'toplevel))
-(gtk-container-border-width window 10)
-(gtk-signal-connect window "delete_event" #'(lambda () (gtk-exit)))
+(let
+    ((window (gtk-window-new 'toplevel))
+     (button (gtk-button-new-with-label "say hello")))
+  (gtk-window-position window 'mouse)
+  (gtk-container-border-width window 10)
+  (gtk-signal-connect
+   window "delete_event"
+   #'(lambda (w) (if (gtk-standalone-p)
+		     (gtk-exit)
+		   (gtk-widget-destroy w))))
+  (gtk-signal-connect
+   button "clicked"
+   #'(lambda () (if (featurep 'jade)
+		    (insert "hello, world\n")
+		  (write standard-output "hello, world\n"))))
+  (gtk-container-add window button)
+  (gtk-widget-show-all window)
+  (when (gtk-standalone-p)
+    (gtk-main)))
 
-(setq button (gtk-button-new-with-label "hello, world"))
-(gtk-signal-connect button "clicked"
-		    #'(lambda () (write standard-output "hello, world\n")))
-(gtk-container-add window button)
-(gtk-widget-show button)
-
-(gtk-widget-show window)
-
-(gtk-main)
-
+;; Local variables:
+;; major-mode: lisp-mode
+;; End:
