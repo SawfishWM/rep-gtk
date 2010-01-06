7a6e78d375db6c7382691219d17cb14821d7aca5
Christopher Roy Bratusek <chris@nanolx.org>
2010-01-06 13:06:31 +0100

    basic GtkTreeView example

---
+++ examples/gtktreeview.jl
@@ -0,0 +1,31 @@
+#! /bin/sh
+exec rep --batch "$0" "$@"
+!#
+
+;;; rep-gtk TreeView example
+
+(structure ()
+
+    (open rep
+	  rep.system
+	  gui.gtk-2.gtk)
+
+  ;; Widgets
+  (define MainWindow (gtk-window-new 'toplevel))
+  (define TreeView (gtk-tree-view-new))
+
+  ;; Windowborder
+  (gtk-container-set-border-width MainWindow 10)
+
+  ;; Exit handling
+  (g-signal-connect MainWindow "delete_event" (lambda (w) (throw 'quit 0)))
+
+  ;; Pack Widgets
+  (gtk-container-add MainWindow TreeView)
+
+  ;; Show widgets
+  (gtk-widget-show-all MainWindow)
+
+  ;; Stuff
+  (setq interrupt-mode 'exit)
+  (recursive-edit))
