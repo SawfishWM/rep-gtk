2e4cb517f2858f4505f70db5c03846752aeecc82
Christopher Roy Bratusek <chris@nanolx.org>
2010-01-07 20:16:50 +0100

    a bit more GtkTreeView/GtkListStore work

--- examples/gtktreeview.jl
+++ examples/gtktreeview.jl
@@ -12,9 +12,23 @@ exec rep --batch "$0" "$@"
 
   ;; Widgets
   (define MainWindow (gtk-window-new 'toplevel))
- ; (define ListStore (gtk-list-store-newv 1 G_TYPE_STRING))
- ; (define TreeView (gtk-tree-view-new-with-model ListStore))
-   (define TreeView (gtk-tree-view-new))
+  (define ListStore (gtk-list-store-newv 1 '(GdkPixbuf) 2 '(gchararray)))
+  (define TreeView (gtk-tree-view-new-with-model ListStore))
+
+  (define PixbufRender (gtk-cell-renderer-pixbuf-new))
+  (define PixbufColumn (gtk-tree-view-column-new))
+  (gtk-tree-view-column-pack-start PixbufColumn PixbufRender t)
+  (gtk-tree-view-column-add-attribute PixbufColumn PixbufRender '("pixbuf") 0)
+  (gtk-tree-view-append-column TreeView PixbufColumn)
+
+  (define TextRender (gtk-cell-renderer-text-new))
+  (define TextColumn (gtk-tree-view-column-new))
+  (gtk-tree-view-column-pack-start TextColumn TextRender t)
+  (gtk-tree-view-column-add-attribute TextColumn TextRender '("text") 1)
+  (gtk-tree-view-append-column TreeView TextColumn)
+
+  (define TestImage (gdk-pixbuf-new-from-file "test.png"))
+  ;(gtk-list-store-append ListStore '(TestImage . "Test"))
 
   ;; Windowborder
   (gtk-container-set-border-width MainWindow 10)
