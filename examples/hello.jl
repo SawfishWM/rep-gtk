290817ca937c5ca11f8f41414b7d6d26b8552841
Christopher Roy Bratusek <nano@tuxfamily.org>
2011-06-02 18:33:51 +0200

    remove GtkComboBoxEntry add GtkComboBoxText bump minimum GTK+ to 2.24

--- examples/hello.jl
+++ examples/hello.jl
@@ -12,13 +12,28 @@ exec rep --batch "$0" "$@"
 
   (define window (gtk-window-new 'toplevel))
 
-  (define button (gtk-scale-button-new 'menu 0 100 0.5 '("gtk-ok" "" "undo")))
+  ;; example for GtkScaleButton                             = 0%     = 100%   = 1% - 99%
+  ;;(define button (gtk-scale-button-new 'menu 0 100 0.5 '("gtk-no" "gtk-yes" "gtk-refresh")))
+
+  ;; example for GtkComboBoxText
+  (define button (gtk-combo-box-text-new))
+  (mapc (lambda (x) (gtk-combo-box-text-append-text button x)) '("111" "222" "333"))
+
+  (define label (gtk-label-new "The selection is shown here"))
+  (define box (gtk-vbox-new nil 0))
+
+  (g-signal-connect button "changed" (lambda ()
+				       (gtk-label-set-text label (gtk-combo-box-text-get-active-text button))))
 
   (gtk-container-set-border-width window 10)
 
   (g-signal-connect window "delete_event" (lambda (w) (throw 'quit 0)))
 
-  (gtk-container-add window button)
+  (gtk-container-add window box)
+
+  (gtk-box-pack-start box label)
+  (gtk-box-pack-start box button)
+
   (gtk-widget-show-all window)
 
   (setq interrupt-mode 'exit)
